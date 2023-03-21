6a54486bd101e4fab04563297190560e457f665f
Leonard Van Iseghem <leonard.vaniseghem@student.hogent.be>
2023-03-21 12:32:23 +0100

    python script voor generating sample data influxdb

---
+++ sampledata.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+import random
+import time
+
+# Define InfluxDB settings
+influxdb_url = "http://localhost:8086"
+influxdb_token = "<your-influxdb-token>"
+influxdb_org = "<your-influxdb-org>"
+influxdb_bucket = "<your-influxdb-bucket>"
+
+# Connect to the InfluxDB client and get a write API
+client = InfluxDBClient(url=influxdb_url, token=influxdb_token, org=influxdb_org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# Define the measurement and tags for the data
+measurement = "temperature"
+location_tag = "location"
+location_value = "room1"
+
+# Generate random temperature data and write it to InfluxDB
+while True:
+    # Generate a random temperature reading between 20 and 30 degrees Celsius
+    temperature_value = random.uniform(20, 30)
+
+    # Get the current time in UTC
+    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Construct the data point
+    data_point = f"{measurement},{location_tag}={location_value} value={temperature_value} {current_time}"
+
+    # Write the data point to InfluxDB
+    write_api.write(influxdb_bucket, record=data_point)
+
+    # Wait for 1 second before generating the next data point
+    time.sleep(1)
--- vic.md
+++ vic.md
@@ -1,8 +1,10 @@
-# VIC 
+# VIC
+
 ## netwerk
+
 1. verbinden met kabel
 
-2. ip in range 10.13.254.0 /16 
+2. ip in range 10.13.254.0 /16
 
 3. default gateway 10.10.0.1
 
@@ -11,6 +13,7 @@
 5. interface resetten
 
 ## vcenter
+
 1. ip in browser zetten
 
 2. [vcenter](10.13.0.50)
@@ -18,25 +21,39 @@
 3. account en wachtwoord in teams
 
 ## nieuwe vm maken
+
 1. onder de 10.13.0.4
-2. create new 
+
+2. create new
+
 3. sandbox
+
 4. 10.13.0.4
-5. powervault
-6. alma linux
-7. datastore iso file
+
+5. gebruik thin provisioning
+
+6. powervault
+
+7. alma linux
+
+8. datastore iso file
 
 ### alma
+
 root leonard321
 
 vi : esc + : + w + q
 
-1. nmcli con mod ens33 ipv4.addresses 192.168.2.20/24    
+1. nmcli con mod ens33 ipv4.addresses 192.168.2.20/24
+
 2. nmcli con mod ens33 ipv4.gateway 192.168.2.1
+
 3. nmcli con mod ens33 ipv4.dns “8.8.8.8”
+
 4. nmcli con mod ens33 ipv4.method manual
+
 5. nmcli connection reload
 
 https://github.com/LeonardVanIseghem01/influxDB-Grafana
 
-docker compose up -d 
+docker compose up -d
